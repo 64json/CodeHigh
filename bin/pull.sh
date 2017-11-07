1bb3266cc5d4defe7c1de08fa4fd5f77487c261b
Jason Park <parkjs814@gmail.com>
2017-11-07 14:21:47 -0600

    Fix webhook

--- bin/pull.sh
+++ bin/pull.sh
@@ -4,6 +4,6 @@ PROJECT_ROOT="$(dirname "$0")/.."
 
 (cd ${PROJECT_ROOT} && git pull && npm install) &&
 
-(chown -R pssde_sl_web: ${PROJECT_ROOT}) &&
+(chown -R ubuntu: ${PROJECT_ROOT}) &&
 
 (pm2 startOrRestart ${PROJECT_ROOT}/pm2.config.js)
